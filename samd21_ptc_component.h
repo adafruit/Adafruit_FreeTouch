5d8c33e084996d48134eaf7d4b200d1a806cb9ee
Scott Shawcroft <scott.shawcroft@gmail.com>
2018-05-07 16:29:27 -0700

    Remove Arduino and ASF dependencies.
    Also, rename CONTROL registers to CTRL to match ASF pattern.

--- samd21_ptc_component.h
+++ samd21_ptc_component.h
@@ -28,12 +28,10 @@
 #ifndef ADAFRUIT_FREETOUCH_PTC_COMPONENT_H
 #define ADAFRUIT_FREETOUCH_PTC_COMPONENT_H
 
-#include <Arduino.h>
+#include "sam.h"
 
-#undef ENABLE
-
-/*************** CONTROL A register ***************/
-#define PTC_REG_CONTROLA   0x42004C00
+/*************** CTRL A register ***************/
+#define PTC_REG_CTRLA   0x42004C00
 #define PTC_BIT_ENABLE     0x02
 #define PTC_BIT_RUNINSTBY  0x04
 
@@ -46,11 +44,11 @@ typedef union {
     uint8_t   __pad0__:5;
   } bit;
   uint8_t reg;
-} PTC_REG_CONTROLA_Type;
+} PTC_REG_CTRLA_Type;
 
-/*************** CONTROL B register ***************/
+/*************** CTRL B register ***************/
 
-#define PTC_REG_CONTROLB 0x42004C01
+#define PTC_REG_CTRLB 0x42004C01
 #define PTC_BIT_SYNCFLAG 0x80
 
 typedef union {
@@ -59,7 +57,7 @@ typedef union {
     uint8_t   SYNCFLAG:1;
   } bit;
   uint8_t reg;
-} PTC_REG_CONTROLB_Type;
+} PTC_REG_CTRLB_Type;
 
 /*************** UNK4C04 register ***************/
 
@@ -70,9 +68,9 @@ typedef union {
 } PTC_REG_UNK4C04_Type;
 
 
-/*************** CONTROL C register ***************/
+/*************** CTRL C register ***************/
 
-#define PTC_REG_CONTROLC 0x42004C05
+#define PTC_REG_CTRLC 0x42004C05
 #define PTC_BIT_INIT 0x01
 
 typedef union {
@@ -81,7 +79,7 @@ typedef union {
     uint8_t   __pad0__:7;
   } bit;
   uint8_t reg;
-} PTC_REG_CONTROLC_Type;
+} PTC_REG_CTRLC_Type;
 
 
 
@@ -107,7 +105,7 @@ typedef union {
 #define PTC_BIT_WCOINTFLAG 0x02
 
 
-/*************** FREQ CONTROL reg ***************/
+/*************** FREQ CTRL reg ***************/
 
 typedef union {
   struct {
@@ -116,13 +114,13 @@ typedef union {
     uint8_t   __pad0__:3;
   } bit;
   uint8_t reg;
-} PTC_REG_FREQCONTROL_Type;
+} PTC_REG_FREQCTRL_Type;
 
-#define PTC_REG_FREQCONTROL 0x42004C0C
+#define PTC_REG_FREQCTRL 0x42004C0C
 #define PTC_BIT_FREQSPREADEN 0x10
 #define PTC_REG_SAMPLEDELAY_MASK 0x0F
 
-/*************** CONVERT CONTROL reg ***************/
+/*************** CONVERT CTRL reg ***************/
 
 typedef union {
   struct {
@@ -131,10 +129,10 @@ typedef union {
     uint8_t   CONVERT:1;
   } bit;
   uint8_t reg;
-} __attribute__ ((packed)) PTC_REG_CONVCONTROL_Type;
+} __attribute__ ((packed)) PTC_REG_CONVCTRL_Type;
 
 
-#define PTC_REG_CONVCONTROL 0x42004C0D
+#define PTC_REG_CONVCTRL 0x42004C0D
 #define PTC_BIT_CONVSTARTED 0x80
 #define PTC_REG_ADCACC_MASK 0x07
 
@@ -302,20 +300,20 @@ typedef union {
 #define PTC_SET_WCO_THRESHHOLD_B_H 0x42004C27
 
 typedef struct {
-  __IO PTC_REG_CONTROLA_Type CONTROLA;    // 0x42004C00
-  __IO PTC_REG_CONTROLB_Type CONTROLB;    // 0x42004C01
+  __IO PTC_REG_CTRLA_Type CTRLA;    // 0x42004C00
+  __IO PTC_REG_CTRLB_Type CTRLB;    // 0x42004C01
   uint8_t   __pad4c02__;                // 0x42004C02 unknown
   uint8_t   __pad4c03__;                // 0x42004C03 unknown
   __IO PTC_REG_UNK4C04_Type  UNK4C04;   // 0x42004C04 unknown
-  __IO PTC_REG_CONTROLC_Type CONTROLC;    // 0x42004C05
+  __IO PTC_REG_CTRLC_Type CTRLC;    // 0x42004C05
   uint8_t   __pad4c06__;                // 0x42004C06 unknown
   uint8_t   __pad4c07__;                // 0x42004C07 unknown
   __IO PTC_REG_INT_Type      INTDISABLE;  // 0x42004C08
   __IO PTC_REG_INT_Type      INTENABLE;   // 0x42004C09
   __IO PTC_REG_INT_Type      INTFLAGS;    // 0x42004C0A
   uint8_t   __pad4c0b__;                // 0x42004C0B unknown
-  __IO PTC_REG_FREQCONTROL_Type FREQCONTROL;  //0x42004C0C
-  __IO PTC_REG_CONVCONTROL_Type CONVCONTROL;  // 0x42004C0D
+  __IO PTC_REG_FREQCTRL_Type FREQCTRL;  //0x42004C0C
+  __IO PTC_REG_CONVCTRL_Type CONVCTRL;  // 0x42004C0D
   uint8_t   __pad4c0e__;                // 0x42004C0E unknown
   uint8_t   __pad4c0f__;                // 0x42004C0F unknown
   __IO PTC_REG_YSELECTL_Type YSELECTL;  // 0x42004C10
@@ -338,7 +336,9 @@ typedef struct {
   __IO PTC_REG_BURSTMODE_Type BURSTMODE; // 0x42004C20
 } Ptc;
 
+#ifndef PTC
 #define PTC  (( Ptc *)0x42004C00U)
+#endif
 
 #define PTC_REG_INTDISABLE 0x42004C08
 #define PTC_REG_INTENABLE 0x42004C09
